62ec4549bcadebb4ebf791aed4f5d3dbd74f261c
Your Name <you@example.com>
2021-11-19 18:21:42 +0000

    Not Ready for deploy 3

--- routes/ninjify.route.js
+++ routes/ninjify.route.js
@@ -7,14 +7,14 @@ router.get('/ninjify', async (req, res) => {
     } else {
         try {
             lst = await req.lst
-            //console.log(req.lst)
-            // const arr = req.query.x.split(",")
-            // var name = arr.map(function(el){
-            //     let obj = lst.find(x => x.param == el.trim())
-            //     if (obj) return obj.name
-            //     return ''
-            // }).join(" ")
-            res.status(200).json({name:lst})
+
+            const arr = req.query.x.split(",")
+            var njname = arr.map(function(el){
+                let obj = lst.find(x => x.param == el.trim())
+                if (obj) return obj.name
+                return ''
+            }).join(" ")
+            res.status(200).json({name:njname})
         } catch (e) {
             console.log(e.message)
             res.status(200).json({message:e.message})
