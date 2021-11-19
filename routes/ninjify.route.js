4ad0165e960ea7cf267a78db0d28db802a4266f9
Your Name <you@example.com>
2021-11-19 17:51:13 +0000

    Added try catch for heroku

--- routes/ninjify.route.js
+++ routes/ninjify.route.js
@@ -5,14 +5,20 @@ router.get('/ninjify', async (req, res) => {
     if (!req.query.x){
         res.status(404).json({message:"Not found"})
     } else {
-        lst = await req.lst
-        const arr = req.query.x.split(",")
-        var name = arr.map(function(el){
-            let obj = lst.find(x => x.param == el.trim())
-            if (obj) return obj.name
-            return ''
-        }).join(" ")
-        res.status(200).json({name})
+        try {
+            lst = await req.lst
+            const arr = req.query.x.split(",")
+            var name = arr.map(function(el){
+                let obj = lst.find(x => x.param == el.trim())
+                if (obj) return obj.name
+                return ''
+            }).join(" ")
+            res.status(200).json({name})
+        } catch (e) {
+            console.log(e.message)
+            res.status(200).json({message:e.message})
+        }
+
     }
 
 })
